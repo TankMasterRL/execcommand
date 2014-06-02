c6ff38e610a277ef7eb8bd8542eff948b0f10b84
TankMasterRL <tankmaster93@gmail.com>
2014-06-02 14:34:34 +0200

    Initial commit

---
+++ execCommand.js
@@ -0,0 +1,30 @@
+var execSync;
+
+//Seeing whether the required dependencies are installed
+try {
+	execSync = require("execsync");
+}
+catch (exception) {
+	//Setting to null, to stop further execution
+	execSync = null;
+}
+
+
+// Executing the code in the statement below if the dependencies are found
+if (execSync !== null)
+{
+	module.exports = function(command) {
+		// Using execsync module as an exception, until child_process.execSync()
+		// arrives in node v0.12.x
+		var result = execSync(command);
+		var end = result.lastIndexOf("\n"); // Removing last nextline character
+		if (end > 0 && end !== null) {
+			result  = result.slice(0, end);
+		}
+		return result;
+	};
+}
+else {
+	console.log("The required dependencies are not installed!");
+	console.log("execsync: %s", execsync !== null ? "Installed" : "NOT INSTALLED");
+}
