0429acab537b399d66152c5a69a93389768ee6ee
TankMasterRL <tankmaster93@gmail.com>
2014-06-17 21:27:59 +0200

    License conformance.

--- execcommand.js
+++ execcommand.js
@@ -16,12 +16,20 @@ if (execSync !== null)
 	module.exports = function(command) {
 		// Using execsync module as an exception, until child_process.execSync()
 		// arrives in node v0.12.x
-		var result = execSync(command);
-		var end = result.lastIndexOf("\n"); // Removing last nextline character
-		if (end > 0 && end !== null) {
-			result  = result.slice(0, end);
+		var result;
+		try {
+			result = execSync(command);
+			
+			var end = result.lastIndexOf("\n"); // Removing last nextline character
+			if (end > 0 && end !== null) { // Null check for result content that is null
+				result  = result.slice(0, end);
+			}
+			return result;
 		}
-		return result;
+		catch (exception) {
+			console.error(exception);
+			return exception;
+		}
 	};
 }
 else {
